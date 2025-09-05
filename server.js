81477e106a2b990792c7ffb4a9ae4952316e1b61
PrajwalVN <prajwalnagle1530@gmail.com>
2025-09-06 00:26:18 +0530

    update

--- server.js
+++ server.js
@@ -24,4 +24,8 @@ app.post("/groq", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Proxy running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Proxy running on http://localhost:${PORT}`);
+});
